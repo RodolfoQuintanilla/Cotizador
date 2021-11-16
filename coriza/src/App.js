a7169a33b806b600a9adc19d21c071fd6a2dbcf0
Rodolfo <rqm@outlook.es>
2021-11-16 16:04:41 -0600

    maquietacion

--- coriza/src/App.js
+++ coriza/src/App.js
@@ -1,12 +1,38 @@
 import { Fragment } from "react";
 import Header from './components/header';
+import Formulario from './components/Formulario';
+
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+max-width: 600px;
+margin: 0 auto;
+`;
+
+const ContenedorFprmulario = styled.div`
+background-color: #FFF;
+padding: 3rem;
+`;
+
 
 function App() {
   return (
     <Fragment>
-      <Header
-        titulo='Cotizador de seguros'
-      />
+      <Contenedor>
+
+        <Header
+          titulo='Cotizador de seguros'
+        />
+
+        <ContenedorFprmulario>
+
+          <Formulario
+
+          />
+
+        </ContenedorFprmulario>
+
+      </Contenedor>
     </Fragment>
   );
 }
--- coriza/src/components/header.js
+++ coriza/src/components/header.js
@@ -1,16 +1,33 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import styled from '@emotion/styled';
+
+
+const ContenedorHeader = styled.header`
+    background-color: #26c6da;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+` ;
+
+const TextoHeader = styled.h1`
+font-size: 2rem;
+margin: 0;
+font-family: 'Slabo 27px', serif;
+text-align: center;
+`
+
+
 
 const Header = ({ titulo }) => {
 
-    //const [state, setstate] = useState(initialState);
 
     return (
 
-        <Fragment>
-            <header>
-                <h1>{titulo}</h1>
-            </header>
-        </Fragment>
+
+        < ContenedorHeader>
+            <TextoHeader> {titulo} </TextoHeader>
+        </ContenedorHeader>
+
 
     );
 
